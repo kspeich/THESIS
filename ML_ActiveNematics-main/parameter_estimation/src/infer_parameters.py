a84f5a9eabb4578f4b2f8b382e0b350a2d210dff
Kodai Speich <kspeich@princeton.edu>
2025-04-21 00:53:48 -0400

    Bug fixes on infer_parameters.py and looping over files

--- ML_ActiveNematics-main/parameter_estimation/src/infer_parameters.py
+++ ML_ActiveNematics-main/parameter_estimation/src/infer_parameters.py
@@ -2,6 +2,7 @@ import torch
 from torch.utils.data import DataLoader
 from torchvision import transforms
 
+import glob
 import argparse
 import numpy as np      # Sometimes DataLoaders are really slow if you import numpy first?
 import pandas as pd
@@ -15,72 +16,76 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 
 # This script can be run on command line by: python estimate_parameters.py --model_name='MODELNAME' --dataset='DATASET'
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_name', type=str)           # This is be the folder path to the trained model (must train the model first)
-    parser.add_argument('--dataset', type=str)              # This is the file path to the dataset
-    parser.add_argument('--output_directory', type=str)     # This is where the code will output a CSV file
+    parser.add_argument('--directory', type=str)            # This is the file path to the input directory with all the datasets
+    parser.add_argument('--output_directory', type=str, default='../../../data/inferences')     # This is where the code will output a CSV file
 
     parser.add_argument('--batch_size', type=int, default=32)
     parser.add_argument('--num_workers', type=int, default=2)
 
     args = parser.parse_args()
 
+    # This is the list of all of the HDF5 files in the input directory
+    paths = glob.glob(f'{args.directory}/*.hdf5')
+
     # GPU or CPU
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     logger.info(f'Training on {device}')
-
+    
     # Create the model
     model = ConvNextParameterEstimator()    # Here we are using the default values
 
     # Load the weights from the trained model
     logger.info(f'Loading weights for {args.model_name}')
     info = torch.load(f'{args.model_name}/model_weight.ckpt', map_location=device, weights_only=True)
-    logger.debug(f'Keys of the weight file: {info.keys()}')
-
     model.load_state_dict(info['state_dict'])           # Put the weights into the model object
     model.to(device)
 
-    # Now we want to read the data
-    inferences = []
+    # Then we loop over every HDF5 file in our input directory
+    for path in paths:
+        logger.info(f'Processing data file: {path}')
 
-    # We include the non-random transformations that we used in training in train_parameter_estimator.py
-    transform = transforms.Compose([
-        Sin2t(),
-        ToTensor(),
-    ])
+        # Now we want to read the data
+        inferences = []
 
-    # Pass the data into the NematicsSequenceDataset object (defined in dataset.py)
-    dataset = NematicsSequenceDataset(path=args.dataset, transform=transform)
-    dataloader = DataLoader(dataset, batch_size=args.batch_size, num_workers=args.num_workers, pin_memory=True)     # We create dataloader to process in batches below
+        # We include the non-random transformations that we used in training in train_parameter_estimator.py
+        transform = transforms.Compose([
+            Sin2t(),
+            ToTensor(),
+        ])
 
-    logger.info('Starting to infer parameters')
-    model.eval()        # Make sure that the model is in evaluation mode
+        # Pass the data into the NematicsSequenceDataset object (defined in dataset.py)
+        dataset = NematicsSequenceDataset(path=path, transform=transform)
+        dataloader = DataLoader(dataset, batch_size=args.batch_size, num_workers=args.num_workers, pin_memory=True)     # We create dataloader to process in batches below
 
-    # Now we iterate over the batches in dataloader
-    with torch.no_grad():
-        counter = 0
-        for batch in dataloader:
-            counter += 1
-            logger.info(f'Processing batch {counter} out of {len(dataloader)}')
-            inputs = batch[0].to(device)
-            logger.debug(f'Input shape: {inputs.shape}, dtype: {inputs.dtype}')
+        logger.info('Starting to infer parameters')
+        model.eval()        # Make sure that the model is in evaluation mode
 
-            outputs = model(inputs)
-            inferences.append(outputs.cpu().numpy())
+        # Now we iterate over the batches in dataloader
+        with torch.no_grad():
+            counter = 0
+            for batch in dataloader:
+                counter += 1
+                logger.info(f'Processing batch {counter} out of {len(dataloader)}')
+                inputs = batch[0].to(device)
+                logger.debug(f'Input shape: {inputs.shape}, dtype: {inputs.dtype}')
 
-    inferences = np.concatenate(inferences, axis=0)
+                outputs = model(inputs)
+                inferences.append(outputs.cpu().numpy())
 
-    # Write the parameter estimates as a DataFrame object, then save as a CSV 
-    labels = ['k', 'z']
-    df_infer = pd.DataFrame(inferences, columns=[l for l in labels])
+        inferences = np.concatenate(inferences, axis=0)
 
-    output_dir = Path(args.output_directory) / 'Inferences'
-    output_dir.mkdir(parents=True, exist_ok=True)         # Create output_dir if it doesn't exist already
-    output_file = f'{output_dir / Path(args.dataset).stem}.csv'
+        # Write the parameter estimates as a DataFrame object, then save as a CSV 
+        df_infer = pd.DataFrame(inferences, columns=['k', 'z'])
 
-    logger.info(f'Saving inferences to {output_file}')
-    df_infer.to_csv(output_file)
+        output_dir = Path(args.output_directory) / 'Inferences'
+        output_dir.mkdir(parents=True, exist_ok=True)         # Create output_dir if it doesn't exist already
+        output_file = f'{output_dir / Path(path).stem}.csv'
+
+        logger.info(f'Saving inferences to {output_file}')
+        df_infer.to_csv(output_file)
